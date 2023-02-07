5e12570ca73f527ffa7f4afbff53637f6debcac9
mevega <mevega@sia.es>
2023-02-07 11:21:51 +0100

    first version

--- src/main.py
+++ src/main.py
@@ -1,4 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import hashlib
+import random
+import string
+
+def generate_hash_sha256(input_string: str) -> str:
+    result = hashlib.sha256(input_string.encode())
+    return result.hexdigest()
+
+def generate_hash_sha256_truncated(input_string: str, size: int) -> str:
+    return truncate(generate_hash_sha256(input_string), size)
+
+def generate_random_string(max_size: int) -> str:
+    return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(max_size))
+
+def truncate(input_string: str, size: int) -> str:
+    if len(input_string) >= size:
+        return input_string[:size]
+    else:
+        return None
 
 
 if __name__ == '__main__':
-    pass
+    print(generate_hash_sha256(str(1)))
+    print(truncate(generate_hash_sha256(str(1)), 50))
+    print(generate_random_string(50))
+    print(generate_random_string(10))
---
+++ tests/main_test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+import main
+
+class TestHashPoC(unittest.TestCase):
+
+    def test_truncate_1(self):
+        self.assertEqual(main.truncate("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b", 5), '6b86b')
+
+    def test_truncate_2(self):
+        self.assertEqual(main.truncate("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b", 50),
+                         '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c0')
+
+    def test_generate_random_string_size(self):
+        size_max: int = 50
+        result: str = main.generate_random_string(size_max)
+        self.assertEqual(len(result), size_max)
+
+    def test_generate_random_string_chars(self):
+        size_max: int = 50
+        result: str = main.generate_random_string(size_max)
+        self.assertEqual(result.isalnum(), True)
+
+    def test_collition_1(self):
+        size_max_string: int = 5
+        size_max_hash_truncate: int = 5
+        result_random_1: str = main.generate_hash_sha256_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
+        result_random_2: str = main.generate_hash_sha256_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
+        while result_random_1 != result_random_2:
+            result_random_1 = main.generate_hash_sha256_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
+            result_random_2 = main.generate_hash_sha256_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
+
+        print("result_random_1:", result_random_1)
+        print("result_random_2:", result_random_2)
+        self.assertEqual(result_random_1, result_random_2)
+
+    def test_collition_2(self):
+        size_max_string: int = 5
+        size_max_hash_truncate: int = 5
+        hash_to_collition = '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b'
+        result_random: str = main.generate_hash_sha256_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
+        result_no_random: str = main.truncate(hash_to_collition, size_max_hash_truncate)
+        while result_random != result_no_random:
+            result_random = main.generate_hash_sha256_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
+
+        print("result_random:", result_random)
+        print("result_no_random:", result_no_random)
+        self.assertEqual(result_random, result_no_random)
+
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+    #
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
