88a1462be5ba0599143f634081c5eaca7e15fb5d
mevega <mevega@sia.es>
2023-02-07 13:52:14 +0100

    fixed and added documentation

--- src/main.py
+++ src/main.py
@@ -5,21 +5,27 @@ import hashlib
 import random
 import string
 
+
 def generate_hash(input_string: str) -> str:
     result = hashlib.sha256(input_string.encode())
     return result.hexdigest()
 
+
 def generate_hash_truncated(input_string: str, size: int) -> str:
     return truncate(generate_hash(input_string), size)
 
+
 def generate_random_string(max_size: int) -> str:
-    #stringLength = 8
-    letters = string.ascii_lowercase
-    preimg = ''.join(random.choice(letters) for i in range(max_size))  # Creamos un preimg aleatorio
+    if max_size > 0:
+        letters = string.ascii_lowercase
+        preimg = ''.join(random.choice(letters) for i in range(max_size))  # Creamos un preimg aleatorio
+    else:
+        return None
     return preimg
 
+
 def truncate(input_string: str, size: int) -> str:
-    if len(input_string) >= size:
+    if len(input_string) >= size and size > 0:
         return input_string[:size]
     else:
         return None
@@ -27,6 +33,7 @@ def truncate(input_string: str, size: int) -> str:
 
 if __name__ == '__main__':
     print(generate_hash(str(1)))
+    print(generate_hash(str(generate_random_string(8))))
     print(truncate(generate_hash(str(1)), 50))
     print(generate_random_string(50))
     print(generate_random_string(10))
--- tests/main_test.py
+++ tests/main_test.py
@@ -4,21 +4,39 @@
 import unittest
 import main
 
-size_max_hash_truncate: int = 4
+size_max_hash_truncate: int = 6
+size_max_string: int = 8
+
 
 class TestHashPoC(unittest.TestCase):
 
-    # def test_truncate_1(self):
-    #     self.assertEqual(main.truncate("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b", 5), '6b86b')
-    #
-    # def test_truncate_2(self):
-    #     self.assertEqual(main.truncate("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b", 50),
-    #                      '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c0')
+    def test_truncate_0(self):
+        self.assertEqual(main.truncate("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b", -5), None)
 
-    def test_generate_random_string_size(self):
+    def test_truncate_1(self):
+        self.assertEqual(main.truncate("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b", 5), '6b86b')
+
+    def test_truncate_2(self):
+        self.assertEqual(main.truncate("6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b", 50),
+                         '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c0')
+
+    def test_generate_random_string_size_1(self):
         size_max: int = 50
         result: str = main.generate_random_string(size_max)
-        self.assertEqual(len(result), size_max)
+        if result is None:
+            result_size = None
+        else:
+            result_size = len(result)
+        self.assertEqual(result_size, size_max)
+
+    def test_generate_random_string_size_2(self):
+        size_max: int = -50
+        result: str = main.generate_random_string(size_max)
+        if result is None:
+            result_size = None
+        else:
+            result_size = len(result)
+        self.assertEqual(result_size, None)
 
     def test_generate_random_string_chars(self):
         size_max: int = 50
@@ -26,41 +44,34 @@ class TestHashPoC(unittest.TestCase):
         self.assertEqual(result.isalnum(), True)
 
     def test_collition_1(self):
-        size_max_string: int = 15
-        #size_max_hash_truncate: int = 8
+        """
+        Check 2 hash generated with random inputs
+        :return:
+        """
         result_random_1: str = main.generate_hash_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
         result_random_2: str = main.generate_hash_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
         while result_random_1 != result_random_2:
             result_random_1 = main.generate_hash_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
             result_random_2 = main.generate_hash_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
 
-            print("result_random_1:", result_random_1)
-            print("result_random_2:", result_random_2)
+        print("result_random_1:", result_random_1)
+        print("result_random_2:", result_random_2)
         self.assertEqual(result_random_1, result_random_2)
 
     def test_collition_2(self):
-        size_max_string: int = 15
-        #size_max_hash_truncate: int = 8
-        hash_to_collition = 'c4ca4238a0b923820dcc509a6f75849b'
+        """
+        Check 2 hash, 1 generated with random input but other is knows
+        :return:
+        """
+        hash_to_collition = '25bff8bbacd4f48a766863c1449f17f11ed841fe49144b628f22abc1d926d1ca'
         result_random: str = main.generate_hash_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
         result_no_random: str = main.truncate(hash_to_collition, size_max_hash_truncate)
         while result_random != result_no_random:
             result_random = main.generate_hash_truncated(main.generate_random_string(size_max_string), size_max_hash_truncate)
 
-            print("result_random:", result_random)
-            print("result_no_random:", result_no_random)
+        print("result_random:", result_random)
+        print("result_no_random:", result_no_random)
         self.assertEqual(result_random, result_no_random)
-
-    # def test_isupper(self):
-    #     self.assertTrue('FOO'.isupper())
-    #     self.assertFalse('Foo'.isupper())
-    #
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
 
 
 if __name__ == '__main__':
